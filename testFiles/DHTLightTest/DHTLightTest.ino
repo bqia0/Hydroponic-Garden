3c4f81232c492bc241e5c93e0ca989e34e44b729
Brandon Qiao <qiaobrandon@gmail.com>
2015-10-05 16:00:10 -0400

    Update
    Full temp/humid descriptions

--- testFiles/DHTLightTest/DHTLightTest.ino
+++ testFiles/DHTLightTest/DHTLightTest.ino
@@ -1,3 +1,9 @@
+#include <DHT.h>
+
+
+
+
+
 // Example testing sketch for various DHT humidity/temperature sensors
 // Written by ladyada, public domain
 
@@ -6,8 +12,8 @@
 #define DHTPIN 2     // what pin we're connected to
 
 // Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302)
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
 // Connect pin 1 (on the left) of the sensor to +5V
