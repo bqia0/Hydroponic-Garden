da4be31d02719642cd4e366cba83b1bc316f4023
Brandon Qiao <qiaobrandon@gmail.com>
2015-12-09 22:33:18 -0500

    LifeSucks

--- testFiles/LightLedTest/LightLedTest.ino
+++ testFiles/LightLedTest/LightLedTest.ino
@@ -1,7 +1,8 @@
 void setup() {
   // put your setup code here, to run once:
-  Serial.print(9600);
-  Serial.print("Begin");
+  Serial.begin(9600);
+  Serial.println("Begin");
+  pinMode(10, OUTPUT);
   pinMode(A0, INPUT);
 }
 
@@ -9,21 +10,21 @@ void loop() {
   // put your main code here, to run repeatedly:
   lights();
   delay(5000);
-  Serial.print("Loop");
+  Serial.println("Loop");
 }
 
 void lights(){
   if (light()>=400){ //change that light value
     digitalWrite(10, LOW);
-    Serial.print("Lights on");
+    Serial.println("Lights on");
   }else{
     digitalWrite(10, HIGH);
-    Serial.print("Lights off");
+    Serial.println("Lights off");
   }
 }
 
 int light(){
-  Serial.print(                                                                                                                                     analogRead(A0));
+  Serial.println(analogRead(A0));
   return analogRead(A0);
   
 }
